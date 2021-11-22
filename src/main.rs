86abc2fcc454d522aeaca701257ef9508b2bce9f
xeeny <xeeny@wp.pl>
2021-11-22 19:18:19 +0100

    implemented pasword generation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use rand::Rng;
+use std::char;
 
-fn generate_password(len: u32) -> String {
-    unimplemented!();
+fn generate_password(charset: &str, len: u32) -> String {
+    let mut result = String::new();
+    let mut rng = rand::thread_rng();
+    for _ in 0..len {
+        let idx = rng.gen_range(0..charset.len());
+        result.push(char::from_u32(charset.as_bytes()[idx] as u32).expect(""));
+    }
+    return result;
 }
 
 fn main() {
-    generate_password(10);
+    println!("{}", generate_password("qwertyuiopasdfghjklzxcvbnm", 10));
 }
-
-
