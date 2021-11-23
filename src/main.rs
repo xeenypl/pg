17dfbb61f3a4a574b521b74f67fa0bf141880263
xeeny <xeeny@wp.pl>
2021-11-23 18:30:54 +0100

    Add argument parser with clap.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use rand::Rng;
 use rand::prelude::ThreadRng;
 use std::char;
+use clap::App;
+use clap::Arg;
+use clap::ArgMatches;
 
 struct PassGen {
     charset: String,
@@ -19,14 +22,47 @@ impl PassGen {
         for _ in 0..len {
             let idx = self.rng.gen_range(0..self.charset.len());
             result.push(
-                char::from_u32(self.charset.as_bytes()[idx] as u32).expect("")
+                // TODO: Clean exepts.
+                char::from_u32(self.charset.as_bytes()[idx] as u32)
+                    .expect("Convertion fail")
             );
         }
         return result;
     }
 }
 
+fn arg_parser<'a>() -> ArgMatches<'a> {
+    App::new("pgn - Command line password generator")
+        .version("0.1.1")
+        .author("Piotr `xeeny` Dudziński")
+        .arg(
+            Arg::with_name("length")
+                .short("l")
+                .long("length")
+                .help("How long posword generate.")
+                .takes_value(true)
+                .value_name("length")
+                .default_value("10")
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("n")
+                .long("count")
+                .help("How many posword generate.")
+                .takes_value(true)
+                .value_name("count")
+                .default_value("1")
+        )
+        .get_matches()
+}
+
 fn main() {
+    let args = arg_parser();
+    // TODO: Clean unwraps.
+    let length: u32 = args.value_of("length").unwrap().parse().unwrap();
+    let count:  u32 = args.value_of("count").unwrap().parse().unwrap();
     let mut pgn = PassGen::new("qwertyuiopasdfghjklzxcvbnm");
-    println!("{}", pgn.gen(10));
+    for _ in 0..count {
+        println!("{}", pgn.gen(length));
+    }
 }
